fbc4e0303a540e57364c5e286b451a67e28f5fba
anideva <anidevarchowdhury@gmial.com>
2026-02-12 12:43:18 +0530

    did correction in js as it was not working with forward and backward

--- script.js
+++ script.js
@@ -2,20 +2,30 @@ const inputs = document.querySelectorAll(".code");
 
 inputs.forEach((input, index) => {
 
-  input.addEventListener("input", (e) => {
+  // Forward typing
+  input.addEventListener("input", () => {
+
+    // Allow only digits
     input.value = input.value.replace(/[^0-9]/g, "");
 
-    if (input.value && index < inputs.length - 1) {
+    // Move forward if value entered
+    if (input.value !== "" && index < inputs.length - 1) {
       inputs[index + 1].focus();
     }
+
   });
 
+  // Backspace logic
   input.addEventListener("keydown", (e) => {
+
     if (e.key === "Backspace") {
-      if (!input.value && index > 0) {
+
+      // If current box is empty, move back
+      if (input.value === "" && index > 0) {
         inputs[index - 1].focus();
       }
     }
+
   });
 
 });
