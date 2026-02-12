ae4512a61e6c3fe769551f1c2211ac6cd429b91b
anideva <anidevarchowdhury@gmial.com>
2026-02-12 12:25:36 +0530

    did all the js work to use the functionality

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll(".code");
+
+inputs.forEach((input, index) => {
+
+  input.addEventListener("input", (e) => {
+    input.value = input.value.replace(/[^0-9]/g, "");
+
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (!input.value && index > 0) {
+        inputs[index - 1].focus();
+      }
+    }
+  });
+
+});
